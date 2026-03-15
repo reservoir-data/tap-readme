d87491fbcfe1e0875a309d3beed5f0fcf192779f
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-03-15 16:50:53 -0600

    feat: Migrate to API v2
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tap_readme/tap.py
+++ tap_readme/tap.py
@@ -2,24 +2,12 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, override
+from typing import override
 
 from singer_sdk import Stream, Tap
 from singer_sdk import typing as th
 
 from tap_readme import streams
-
-if TYPE_CHECKING:
-    from urllib.parse import ParseResult
-
-    from singer_sdk.streams import RESTStream
-
-STREAM_TYPES: list[type[RESTStream[ParseResult]]] = [
-    streams.Categories,
-    streams.CategoryDocs,
-    streams.Metadata,
-    streams.Changelogs,
-]
 
 
 class TapReadMe(Tap):
@@ -39,4 +27,8 @@ class TapReadMe(Tap):
 
     @override
     def discover_streams(self) -> list[Stream]:
-        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
+        return [
+            streams.Branches(tap=self),
+            streams.Categories(tap=self),
+            streams.Changelogs(tap=self),
+        ]
